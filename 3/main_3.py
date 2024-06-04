f3426ac2710e8a8559cd6d3d96cf7ef70364fc08
Olesia Arkhishina <143208496+Olesia1405@users.noreply.github.com>
2024-06-04 11:03:55 +0300

    Доработка задачи #3

--- 3/main_3.py
+++ 3/main_3.py
@@ -1,13 +1,29 @@
-with (open('2.txt', 'r', encoding='utf-8') as f2,
-      open('1.txt', 'r', encoding='utf-8') as f1,
-      open('3.txt', 'r', encoding='utf-8') as f3,
-      open('output.txt', 'w', encoding='utf-8') as f):
-    files = [('file2.txt', f2), ('file1.txt', f1), ('file3.txt', f3)]
-    for file_name, file in files:
-        lines = file.readlines()
-        line_count = len(lines)
-        f.write(f"{file_name}\n")
-        f.write(f"{line_count}\n")
-        for i, line in enumerate(lines, start=1):
-            f.write(f"Строка номер {i} файла номер {file_name[4:]}: {line}")
+import os
 
+folder_path = os.path.abspath('.')
+file_names = []
+for file_name in os.listdir(folder_path):
+    if os.path.isfile(os.path.join(folder_path, file_name)):
+        if file_name.find('.txt') != -1 and file_name[0].isdigit():
+            file_names.append(file_name)
+print(file_names)
+
+info_files = {}
+for file in range(len(file_names)):
+    with open(file_names[file], 'r') as f:
+        lines = f.readlines()
+        line_count = len(lines)
+        f.seek(0)
+        a = f.read()
+        info_files[f.name] = [line_count, a]
+
+sort_files = {}
+for i in sorted(info_files.items(), key=lambda para: para[1][0]):
+    sort_files[i[0]] = [i[1][0], i[1][1]]
+print(sort_files)
+
+for file in sort_files:
+    with open(file, 'r') as fn, open('output.txt', 'a+') as f:
+        fn.seek(0)
+        a = fn.read()
+        f.write(f'{file}\n{sort_files[file][0]}\n{a}\n')
